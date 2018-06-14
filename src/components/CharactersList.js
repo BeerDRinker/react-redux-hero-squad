0e9f3bc5d342faafd6749a2e062bebcca5d0352e
BeerDrinker <paliy1984@gmail.com>
2018-06-14 21:23:58 +0300

    from notebook

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,47 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import { bindActionCreators } from 'redux';
-
-import { addCharacterById } from '../actions/index';
 
 class CharactersList extends Component {
 	render() {
 		return (
 			<div>
-				<h2>Characters</h2>
-				<ul>
-					{
-						this.props.characters.map(character => {
-							return (
-								<li key={character.id}>
-									<div>{character.name}</div>
-									<div onClick={() => this.props.addCharacterById(character.id)}>
-										x
-									</div>
-								</li>
-							);
-						})
-					}
-				</ul>
+				<h4>Characters</h4>
 			</div>
 		);
 	}
 }
 
-function mapStateToProps(state) {
-	return {
-		characters: state.characters
-	};
-}
-
-
-// FULL SINTAX FOR mapDispatchToPRops
-
-// function mapDispatchToProps(dispatch) {
-// 	return bindActionCreators({ addCharacterById }, dispatch)
-// }
-// export default connect(mapStateToProps, mapDispatchToProps)(CharactersList);
-
-// ShortCut for mapDispatch to props
-
-export default connect(mapStateToProps, { addCharacterById })(CharactersList);
+export default CharactersList;
